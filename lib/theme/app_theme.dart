3cd4484cda5c39e8454ad373f0bf8bd72dd8e02a
ffchrulrozi <ffchrulrozi@gmail.com>
2025-07-18 09:20:31 +0900

    set base code

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appTheme = ThemeData(
+  scaffoldBackgroundColor: Colors.white,
+  appBarTheme: AppBarTheme(
+    backgroundColor: Colors.indigo,
+    foregroundColor: Colors.white,
+  ),
+);
